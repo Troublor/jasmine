2ae6203872a80be142c3ca8b50e4a1f791dd51ea
Aaron <troublor@live.com>
2020-09-22 17:04:57 +0800

    add jasmine-eth-ts submodule

--- scripts/build_contracts_for_sdk.ts
+++ scripts/build_contracts_for_sdk.ts
@@ -13,21 +13,27 @@ if (result.status !== 0) {
 }
 
 // save abi to a temp file
-let tmp = path.join(__dirname, "tmp", "contract.json");
+let abi = path.join(__dirname, "tmp", "contract.abi");
+let bin = path.join(__dirname, "tmp", "contract.bin");
 let contractBuild = JSON.parse(fs.readFileSync(
     path.join(__dirname, "..", "packages", "jasmine-contracts", "build", "contracts", "TFCToken.json")
 ).toString('utf8'));
-fs.writeFileSync(tmp, JSON.stringify(contractBuild['abi']));
+fs.writeFileSync(abi, JSON.stringify(contractBuild['abi']));
+fs.writeFileSync(bin, contractBuild['bytecode']);
 
-console.log("Building golang contract dependency for TFCToken")
+console.log("Building golang contract dependency for TFCToken");
 
 // build go dependency with abigen
 let goPackage = "token"
 let output = path.join(__dirname, "..", "packages", "jasmine-eth-go", "token", "TFCToken.go")
 let typeName = "TFCToken"
-result = child_process.spawnSync("abigen", [`--abi=${tmp}`, `--pkg=${goPackage}`, `--type=${typeName}`, `--out=${output}`], {
-    cwd: __dirname,
-});
+result = child_process.spawnSync(
+    "abigen",
+    [`--bin=${bin}`, `--abi=${abi}`, `--pkg=${goPackage}`, `--type=${typeName}`, `--out=${output}`],
+    {
+        cwd: __dirname,
+    }
+);
 console.log(result.stdout.toString())
 if (result.status !== 0) {
     console.error(result.stderr.toString());
@@ -35,3 +41,23 @@ if (result.status !== 0) {
 }
 
 console.log("Dependency generated at", output);
+
+console.log("Building typescript contract dependency for TFCToken");
+
+// build typescript dependency with typechain
+let outputDir = path.join(__dirname, "..", "packages", "jasmine-eth-ts", "src", "contracts")
+let abis = path.join(__dirname, "..", "packages", "jasmine-contracts", "build", "contracts", "*.json");
+result = child_process.spawnSync(
+    "typechain",
+    ["--target", "web3-v1", "--outDir", outputDir, abis],
+    {
+        cwd: __dirname,
+    }
+);
+console.log(result.stdout.toString())
+if (result.status !== 0) {
+    console.error(result.stderr.toString());
+    process.exit(result.status);
+}
+
+console.log("Dependency generated at", outputDir);

f513b5f882c4e0f4eebdae19424e0a4d346a71e8
Aaron <troublor@live.com>
2020-09-29 22:04:38 +0800

    add TFCManager in SDK

--- scripts/build_contracts_for_sdk.ts
+++ scripts/build_contracts_for_sdk.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as child_process from "child_process";
 import * as path from "path";
 
+console.log("Compiling smart contracts");
 // compile contracts
 let result = child_process.spawnSync("yarn", ["workspace", "jasmine-contracts", "build"], {
     cwd: path.join(__dirname, ".."),
@@ -11,36 +12,6 @@ if (result.status !== 0) {
     console.error(result.stderr.toString());
     process.exit(result.status);
 }
-
-// save abi to a temp file
-let abi = path.join(__dirname, "tmp", "contract.abi");
-let bin = path.join(__dirname, "tmp", "contract.bin");
-let contractBuild = JSON.parse(fs.readFileSync(
-    path.join(__dirname, "..", "packages", "jasmine-contracts", "build", "contracts", "TFCToken.json")
-).toString('utf8'));
-fs.writeFileSync(abi, JSON.stringify(contractBuild['abi']));
-fs.writeFileSync(bin, contractBuild['bytecode']);
-
-console.log("Building golang contract dependency for TFCToken");
-
-// build go dependency with abigen
-let goPackage = "token"
-let output = path.join(__dirname, "..", "packages", "jasmine-eth-go", "token", "TFCToken.go")
-let typeName = "TFCToken"
-result = child_process.spawnSync(
-    "abigen",
-    [`--bin=${bin}`, `--abi=${abi}`, `--pkg=${goPackage}`, `--type=${typeName}`, `--out=${output}`],
-    {
-        cwd: __dirname,
-    }
-);
-console.log(result.stdout.toString())
-if (result.status !== 0) {
-    console.error(result.stderr.toString());
-    process.exit(result.status);
-}
-
-console.log("Dependency generated at", output);
 
 console.log("Building typescript contract dependency for TFCToken");
 
@@ -60,13 +31,44 @@ if (result.status !== 0) {
     process.exit(result.status);
 }
 
-fs.writeFileSync(
-    path.join(__dirname, "..", "packages", "jasmine-eth-ts", "src", "contracts", "TFCToken.abi.json"),
-    JSON.stringify(contractBuild['abi'], null, 2),
-);
-fs.writeFileSync(
-    path.join(__dirname, "..", "packages", "jasmine-eth-ts", "src", "contracts", "TFCToken.bin"),
-    contractBuild['bytecode'],
-);
+let actionForEachContract = (contractName: string) => {
+    // save abi to a temp file
+    let abi = path.join(__dirname, "tmp", `${contractName}.abi`);
+    let bin = path.join(__dirname, "tmp", `${contractName}.bin`);
+    let contractBuild = JSON.parse(fs.readFileSync(
+        path.join(__dirname, "..", "packages", "jasmine-contracts", "build", "contracts", `${contractName}.json`)
+    ).toString('utf8'));
+    fs.writeFileSync(abi, JSON.stringify(contractBuild['abi']));
+    fs.writeFileSync(bin, contractBuild['bytecode']);
 
-console.log("Dependency generated at", outputDir);
+    console.log("Building golang contract dependency for TFCToken");
+
+    let goPackage = "token"
+    let output = path.join(__dirname, "..", "packages", "jasmine-eth-go", "token", `${contractName}.go`)
+    result = child_process.spawnSync(
+        "abigen",
+        [`--bin=${bin}`, `--abi=${abi}`, `--pkg=${goPackage}`, `--type=${contractName}`, `--out=${output}`],
+        {
+            cwd: __dirname,
+        }
+    );
+    console.log(result.stdout.toString())
+    if (result.status !== 0) {
+        console.error(result.stderr.toString());
+        process.exit(result.status);
+    }
+
+    console.log("Copying bytecode and ABI for TFCToken");
+
+    fs.writeFileSync(
+        path.join(__dirname, "..", "packages", "jasmine-eth-ts", "src", "contracts", `${contractName}.abi.json`),
+        JSON.stringify(contractBuild['abi'], null, 2),
+    );
+    fs.writeFileSync(
+        path.join(__dirname, "..", "packages", "jasmine-eth-ts", "src", "contracts", `${contractName}.bin`),
+        contractBuild['bytecode'],
+    );
+};
+
+actionForEachContract("TFCToken");
+actionForEachContract("TFCManager");

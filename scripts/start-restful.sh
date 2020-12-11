787b58caa3b3f84635c2e7f711f5a2bdb921ce27
Aaron <troublor@live.com>
2020-12-12 01:25:17 +0800

    integrate

---
+++ scripts/start-restful.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+# check if restful submodule has been pulled
+if [ ! -f "$DIR"/../packages/jasmine-restful/package.json ]; then
+  git submodule update --init --recursive
+fi
+
+# install node.js dependency
+yarn
+
+if [ ! -f "$DIR"/../restful.config.yml ]; then
+  echo "RESTful service configuration file not found. It should be at PROJECT_ROOT/restful.config.yml"
+  exit 1
+fi
+
+# copy restful.config
+cp "$DIR"/../restful.config.yml "$DIR"/../packages/jasmine-restful/config.yml
+
+yarn workspace jasmine-restful install
+yarn workspace jasmine-restful start
